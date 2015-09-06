11a143b2428c0ad7b16bd04d7f1dd8fefa813c97
J.T. Keller <jtkeller2@gmail.com>
2015-09-06 11:13:55 -0400

    Initial Commit

---
+++ course-project/src/main/java/com/thoughtstreamllc/mvc/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.thoughtstreamllc.mvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/home")
+	public String goHome(){
+		//logical view name, represent one of the views
+		return "home";
+	}
+}
